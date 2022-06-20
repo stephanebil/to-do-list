c9894c2e72cc6ada5e30c737dd91701f1033d6a0
stephanebil <stephanebilella@yahoo.fr>
2022-06-20 10:58:07 +0200

    delete and done task

--- js/script.js
+++ js/script.js
@@ -21,14 +21,37 @@ document.getElementById("push").onclick = function(){
     } else if (elInput.length > 35){ 
         alert("tache trop longue");
     } else {
-        console.log ("tout est ok")
-        // selectionne la div où les taches vont être affichés
-        document.querySelector("#tasks").innerHTML += 
-        `<div class=" d-flex justify-content-between align-items-center mb-3">
-        <span class="my-0" >${elInput}</span>
-        <button class="btn btn-danger">Delete</button>
-        </div>`
-        ;
+      console.log("tout est ok");
+      // selectionne la div où les taches vont être affichés
+      document.querySelector(
+        "#tasks"
+      ).innerHTML += `<div class=" d-flex justify-content-between align-items-center mb-3 border-bottom py-3">
+        <span class="my-0 taskname" >${elInput}</span>
+        <button class="btn btn-danger delete">Delete</button>
+        </div>`;
+      
+      // delete task (attention rajouter classe .delete ci dessus)
+      let tasksDelete = document.querySelectorAll(".delete");
+      console.log("tasksDelete", tasksDelete);
+      for (let i = 0; i < tasksDelete.length; i++) {
+        tasksDelete[i].onclick = function () {
+          console.log("click delete");
+          // selectionne moi le noeud parent et efface moi tout
+          this.parentNode.remove();
+        };
+      }
+
+      // task done (attention rajouter classe .taskname ci dessus
+      let tasksDone = document.querySelectorAll(".taskname");
+      for (let i = 0; i < tasksDone.length; i++) {
+        tasksDone[i].onclick = function () {
+          console.log("task done");
+          // créer une classe completed sur css (voir css)
+          this.classList.toggle("completed")
+        };
+      }
+    // lorsqu'on appuie sur Ajouter la valeur dans todo-input revient à 0
+      document.querySelector("#todo-input").value=""
     }
 };
 
